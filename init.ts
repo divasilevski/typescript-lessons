21f53c3a0115c0f902b0dbe36322222be4f5aed5
Di <divasilevski@gmail.com>
2020-07-05 14:28:38 +0300

    npm init | tsc --init | npm i -D typescript

--- init.ts
+++ init.ts
@@ -2,5 +2,15 @@ const s: string = "Hello world!";
 console.log(s);
 
 // npm i -g typescript
+
+// easy way
 // tsc init.ts
 // node init.js
+
+// advanced way
+// npm init -y
+// npm i -D typescript
+// tsc --init
+
+
+// create index.html + add css framework
