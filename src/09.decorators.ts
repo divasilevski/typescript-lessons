4407bcf1652dce8317bbd8dd4325e8209a71a7bb
Di <divasilevski@gmail.com>
2020-07-06 22:42:50 +0300

    validator - decorator

--- src/09.decorators.ts
+++ src/09.decorators.ts
@@ -90,5 +90,56 @@ class CardComponent {
 const card = new CardComponent("My card component")
 
 const BTN = document.querySelector('#btn')!
-
 BTN.addEventListener('click', card.logName)
+
+
+
+// vlidator
+type ValidatorType = 'required' | 'email'
+
+interface ValidatorConfig {
+  [prop: string]: {
+    [validateProp: string]: ValidatorType
+  }
+}
+
+const validators: ValidatorConfig = {}
+
+function Required(target: any, propName: string) {
+  validators[target.constructor.name] = {
+    ...validators[target.constructor.name],
+    [propName]: 'required'
+  }
+}
+
+function validate(obj: any): boolean {
+  const objConfig = validators[obj.constructor.name]
+  if (!objConfig) {
+    return true
+  }
+
+  let isValid = true;
+  Object.keys(objConfig).forEach(key => {
+    if (objConfig[key] === 'required') {
+      isValid = isValid && !!obj[key]
+    }
+  })
+  return isValid
+}
+
+class Form {
+  @Required
+  public email: string | void
+
+  constructor(email?: string) {
+    this.email = email
+  }
+}
+
+const form = new Form('ewe')
+
+if (validate(form)) {
+  console.log('Valid', form)
+} else {
+  console.log('Validation Error')
+}
