4ab0ab43ad463148803194be667df4ac8cb20ff6
Di <divasilevski@gmail.com>
2020-07-08 21:18:30 +0300

    Interfaces from handbook

--- src/02.interfaces.ts
+++ src/02.interfaces.ts
@@ -1,6 +1,6 @@
 interface Rect {
   readonly id: string;
-  color?: string;
+  color?: string; // optional property
   size: {
     width: number;
     height: number;
@@ -31,7 +31,7 @@ const rect4 = <Rect>{};
 
 
 
-//--------------------//
+// EXTENDS another interface
 interface RectWithArea extends Rect {
   getArea: () => number;
 }
@@ -49,7 +49,7 @@ const rect5: RectWithArea = {
 
 
 
-//--------------------//
+// IMPLEMENTS class
 interface IClock {
   time: Date;
   setTime(date: Date): void;
@@ -65,7 +65,7 @@ class Clock implements IClock {
 
 
 
-//--------------------//
+// [key: type]
 interface Styles {
   [key: string]: string;
 }
@@ -75,3 +75,48 @@ const css: Styles = {
   marginTop: "2px",
   borderRadius: "5px"
 }
+
+
+
+// From handbook
+let ro1: ReadonlyArray<number> = [1, 2, 3, 4];
+let ro2 = ro1 as number[];
+
+
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+let mySearch: SearchFunc = function(src, sub) {
+    let result = src.search(sub);
+    return result > -1;
+}
+
+
+interface StringArray {
+  [index: number]: string;
+}
+let myArray: StringArray = ["Bob", "Fred"];
+let myStr: string = myArray[0];
+
+
+interface NumberOrStringDictionary {
+  [index: string]: number | string;
+  length: number;    // ok, length is a number
+  name: string;      // ok, name is a string
+}
+
+
+interface ClockConstructor {
+  new (hour: number, minute: number): any;
+}
+interface ClockInterface {
+  tick() : void
+}
+const ClockNotStatic: ClockConstructor = class Clock implements ClockInterface {
+  constructor(h: number, m: number) {}
+  tick() {
+      console.log("beep beep");
+  }
+}
+
+
