8d295ae3d291ce1acb1e74368103b3386b1855ac
Di <divasilevski@gmail.com>
2020-07-09 13:33:16 +0300

    Classes From handbook

--- src/05.classes.ts
+++ src/05.classes.ts
@@ -59,7 +59,7 @@ console.log(cat.color);
 
 
 
-//---------------//
+// ABSTRACT classes
 abstract class Component {
   abstract render(): void;
   abstract info(): string;
@@ -74,3 +74,54 @@ class AppComponent extends Component {
     return "This is info!"
   }
 }
+
+
+
+// From handbook
+class Animal2 {
+  name: string;
+  constructor(theName: string) { this.name = theName; }
+  move(distanceInMeters: number = 0) {
+      console.log(`${this.name} moved ${distanceInMeters}m.`);
+  }
+}
+
+class Snake extends Animal2 {
+  constructor(name: string) { super(name); }
+  move(distanceInMeters = 5) {
+      console.log("Slithering...");
+      super.move(distanceInMeters);
+  }
+}
+
+
+class Animal3 {
+  #name: string; // private #
+  constructor(theName: string) { this.#name = theName; }
+}
+
+
+class Person5 {
+  static payment: number = 5000;
+  protected name5: string;
+  protected constructor(theName: string) { this.name5 = theName; } // but we have ascess in expendng classes
+}
+
+
+class Employee5 {
+  private _fullName: string = "Default Employee";
+
+  get fullName(): string {
+      return this._fullName;
+  }
+
+  set fullName(newName: string) {
+      if (newName && newName.length > 10) {
+          throw new Error("fullName has a max length of " + 10);
+      }
+      
+      this._fullName = newName;
+  }
+}
+
+
