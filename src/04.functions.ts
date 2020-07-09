53ce840bff9997d77d38af3484335281b9ebf75d
Di <divasilevski@gmail.com>
2020-07-09 17:18:35 +0300

    Functions from handbook

--- src/04.functions.ts
+++ src/04.functions.ts
@@ -7,6 +7,7 @@ function toUpperCase(str: string): string {
 }
 
 
+
 interface MyPosition {
   x: number | undefined;
   y: number | undefined;
@@ -22,16 +23,53 @@ function position(a: number, b: number): MyPosition;
 
 function position(a?: number, b?: number) {
   if (!a && !b) {
-    return {x: undefined, y: undefined};
+    return { x: undefined, y: undefined };
   }
 
   if (a && !b) {
-    return {x: a, y: undefined, default: a.toString()};
+    return { x: a, y: undefined, default: a.toString() };
   }
 
-  return {x: a, y: b};
+  return { x: a, y: b };
 }
 
 console.log("Empty ", position());
 console.log("One param ", position(7));
 console.log("Two param ", position(7, 14));
+
+
+
+// From handbook
+function buildName1(firstName: string, lastName?: string) { }  // optional parameters
+
+function buildName2(firstName: string, ...restOfName: string[]) { // rest parameters
+  return firstName + " " + restOfName.join(" ");
+}
+
+function f(this: void) { } // make sure `this` is unusable in this standalone function
+
+interface Card {
+  suit: string;
+  card: number;
+}
+interface Deck {
+  suits: string[];
+  cards: number[];
+  createCardPicker(this: Deck): () => Card; // this Deck / we can transfer context to func
+}
+let deck: Deck = {
+  suits: ["hearts", "spades", "clubs", "diamonds"],
+  cards: Array(52),
+  createCardPicker: function(this: Deck) {
+      return () => {
+          let pickedCard = Math.floor(Math.random() * 52);
+          let pickedSuit = Math.floor(pickedCard / 13);
+
+          return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+      }
+  }
+}
+
+interface UIElement { // callback example | this is optional
+  addClickListener(onclick: (this: void, e: Event) => void): void;
+}
