6afd2f7086309a7b6caf14f325ef2a3f9c7a8631
Di <divasilevski@gmail.com>
2020-07-09 17:58:26 +0300

    enums from handbook

--- src/03.enums.ts
+++ src/03.enums.ts
@@ -14,3 +14,44 @@ enum SocialMedia {
 }
 
 const social = SocialMedia.INSTAGRAM;
+
+
+
+// from handbook
+// there are constant and computed enums:
+enum FileAccess {
+  // constant members
+  None,
+  Read    = 1 << 1,
+  Write   = 1 << 2,
+  ReadWrite  = Read | Write,
+  // computed member
+  G = "123".length
+}
+
+
+
+enum LogLevel {
+  ERROR, WARN, INFO, DEBUG
+}
+
+type LogLevelStrings = keyof typeof LogLevel;
+
+
+
+const enum Directions { // const enums
+  Up,
+  Down,
+  Left,
+  Right
+}
+
+let directions = [Directions.Up, Directions.Down, Directions.Left, Directions.Right]
+
+
+
+declare enum Enum { // Ambient enums 
+  A = 1,
+  B,
+  C = 2
+}
