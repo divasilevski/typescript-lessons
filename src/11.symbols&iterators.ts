860c648061ad0aec02d4f5c9e9865c2e47251c54
Di <divasilevski@gmail.com>
2020-07-11 13:39:33 +0300

    iterators

--- src/11.symbols&iterators.ts
+++ src/11.symbols&iterators.ts
@@ -1,3 +1,4 @@
+// SYMBOLS
 let sym1 = Symbol();
 
 let sym2 = Symbol("key"); // optional string key
@@ -15,3 +16,16 @@ class CCC {
 
 let ccc:any = new CCC();
 let className = ccc[sym1](); // "C"
+
+
+
+// ITERATORS
+let list = [4, 5, 6];
+
+for (let i in list) {
+    console.log(i); // "0", "1", "2",
+}
+
+for (let i of list) {
+    console.log(i); // "4", "5", "6"
+}
